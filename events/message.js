08e1490d89d94a59f11a33e2cf2e6661ad51da69
LuciHusband <yilmazmevlut397@gmail.com>
2023-08-25 23:48:38 +0300

    basic bot done

---
+++ events/message.js
@@ -0,0 +1,9 @@
+// const client = require('../index.js').client;
+
+// client.on('messageCreate', async message => {
+//     if (message.author.bot) return;
+    
+//     if (message.content === 'ping') {
+//         await message.reply('Pong!');
+//     }
+// });
--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.login(process.env.DISCORD_TOKEN);
+require('dotenv').config()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
