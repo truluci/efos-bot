417cf5c635dcb61859aa74ca777f95cc59c2686b
LuciHusband <yilmazmevlut397@gmail.com>
2023-08-26 23:24:34 +0300

    :(

--- events/message.js
+++ events/message.js
@@ -1,9 +1,20 @@
-// const client = require('../index.js').client;
-
-// client.on('messageCreate', async message => {
-//     if (message.author.bot) return;
-    
-//     if (message.content === 'ping') {
-//         await message.reply('Pong!');
-//     }
-// });
+const responses = {
+    "hello": ["Hi there!", "Hello!", "Hey!"],
+    "goodbye": ["Goodbye!", "See you later!", "Farewell!"],
+    // Add more triggering words and their responses here
+  };
+  
+  module.exports = {
+    handleResponses: function(message) {
+      for (const trigger in responses) {
+        if (message.content.toLowerCase().includes(trigger)) {
+          const responseOptions = responses[trigger];
+          const selectedResponse = responseOptions[Math.floor(Math.random() * responseOptions.length)];
+          message.channel.send(selectedResponse);
+          console.log(`Triggered response: ${trigger}`);
+          break; // Exit the loop after sending a response
+        }
+      }
+    }
+  };
+  
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// index.js
 
-require('dotenv').config()
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent] });
+
+require('dotenv').config();
+
+const { handleResponses } = require('./events/message'); // Adjust the path as needed
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
+client.on('message', message => {
+  if (message.author.bot) return; // Ignore messages from bots
+  handleResponses(message); // Call the function to handle responses
 });
 
 client.login(process.env.TOKEN);
