0f2b4c4845e6e2a62168da10f8cbcd002bca95cf
LuciHusband <yilmazmevlut397@gmail.com>
2023-08-27 16:22:16 +0300

    responses

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,12 +1,26 @@
 const client = require("../client");
 
+// Define your keyword-response mapping
+const keywordResponses = {
+  "ping": "pong",
+  "bro": "who got you smiling like that",
+  "selam": "selammmm",
+  // Add more keywords and responses here
+};
+
 module.exports = async (message) => {
   if (!message.guild) return;
   if (message.author.bot) return;
   
-  if (message.content === "ping") {
-    message.reply("pong");
-  };
+  const content = message.content.toLowerCase(); // Convert to lowercase for case-insensitive matching
+  
+  // Check if the content matches any keywords
+  for (const keyword in keywordResponses) {
+    if (content.includes(keyword)) {
+      message.reply(keywordResponses[keyword]);
+      break; // Exit loop after the first match
+    }
+  }
 
   console.log(message.content);
 };
