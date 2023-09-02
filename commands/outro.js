2ebfdabcbef21f764ab605ad0c9cd5cfe73e7b20
Necip <sagiroglun22@itu.edu.tr>
2023-09-02 13:52:42 +0300

    refactor: change required variable name to DiscordVoice

--- commands/outro.js
+++ commands/outro.js
@@ -1,31 +1,31 @@
-const { createAudioPlayer, joinVoiceChannel, createAudioResource } = require('@discordjs/voice');
-const path = require('path');
+const DiscordVoice = require('@discordjs/voice');
 const sleep = require('util').promisify(setTimeout);
+const path = require('path');
 
 module.exports = async (message) => {
   if (!message.mentions.users.size) {
     message.reply('birisini etiketle amk düzgün kullan.');
     return;
-  }
+  };
 
   const member = message.guild.members.cache.get(message.mentions.users.first()?.id);
 
   if (!member || !member.voice.channel) {
     message.reply('kanalda yok ki amk düzgün kullan.');
     return;
-  }
+  };
 
   const voiceChannel = member.voice.channel;
 
-  const connection = joinVoiceChannel({
+  const connection = DiscordVoice.joinVoiceChannel({
     channelId: voiceChannel.id,
     guildId: voiceChannel.guild.id,
     adapterCreator: voiceChannel.guild.voiceAdapterCreator,
   });
 
-  const resource = createAudioResource(path.join(__dirname, '../assets/outro.mp3'));
+  const resource = DiscordVoice.createAudioResource(path.join(__dirname, '../assets/outro.mp3'));
 
-  const player = createAudioPlayer();
+  const player = DiscordVoice.createAudioPlayer();
 
   connection.subscribe(player);
 
