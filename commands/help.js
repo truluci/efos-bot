ff7d5b18c9b9c2c6a6a5b8db805d206a51d7a744
Necip <sagiroglun22@itu.edu.tr>
2023-09-06 16:13:44 +0300

    feat: add unknown command alert to help command

--- commands/help.js
+++ commands/help.js
@@ -11,27 +11,34 @@ module.exports = {
     const embed = new Discord.EmbedBuilder()
       .setTitle(this.name)
       .setDescription(this.description);
-
+    
+    const commands = [];
+    
     fs.readdirSync('./commands').forEach(file => {
       const command = require(`./${file}`);
 
-      if (!command.name || command.name == this.name) return;
+      if (command.name && command.name != this.name)
+        commands.push(command);
+    });
 
-      if (content[1]) {
-        if (command.triggers.includes(content[1])) {
-          embed.setTitle(command.name);
-          embed.setDescription(command.detailedDescription || command.description);
-          return;
-        } else {
-          embed.setDescription('Command not found. Use `help` to get a list of commands.');
-        };
-      } else {
+    if (!content[1]) {
+      commands.forEach(command => {
         embed.addFields({
           name: command.name,
           value: command.description
         });
+      });
+    } else {
+      const command = commands.find(command => command.triggers.includes(content[1]));
+
+      if (command) {
+        embed.setTitle(command.name);
+        embed.setDescription(command.detailedDescription || command.description);
+      } else {
+        embed.setTitle('Error');
+        embed.setDescription('Command not found. Please use `help` to see all commands.');
       };
-    });
+    };
 
     message.reply({ embeds: [embed] });
   }
