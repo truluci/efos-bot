2c1d3f97f0015ab415be0bc0ea44e216c12c98fd
Necip <sagiroglun22@itu.edu.tr>
2023-10-24 13:44:05 +0300

    fix: arrangable schedule

--- cron/Job.js
+++ cron/Job.js
@@ -1,11 +1,15 @@
 const cron = require('node-cron');
+const fs = require('fs');
+const path = require('path');
 
 const getVideos = require('./functions/getVideos');
 const hasNewVideo = require('./functions/hasNewVideo');
 const writeVideosToFile = require('./functions/writeVideosToFile');
 const sendTelegramMessage = require('./functions/sendTelegramMessage');
 
-const Job = cron.schedule('0 * * * *', () => {
+const schedule = fs.readFileSync(path.join(__dirname, './data/schedule.txt'), 'utf-8');
+
+const Job = cron.schedule(schedule, () => {
   getVideos((err, videos) => {
     if (err) return console.log(err);
 
