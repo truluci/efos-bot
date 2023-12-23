bf39ad9bb6befb326f438dbdc9caeece674108b2
LuciHusband <yilmazmevlut397@gmail.com>
2023-12-23 12:09:25 +0300

    Fix formatting in vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -17,7 +17,7 @@ module.exports = {
     };
 
     const affirmation = message.content.slice(this.triggers[0].length + 1).trim().toLocaleUpperCase('tr');
-    
+
     startVote(message, {
       description: affirmation,
       reactions: {
